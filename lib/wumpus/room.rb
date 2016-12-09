29423488aca102def4051e8bfe7732892e59a00e
dmozzoni <cyric@hotmail.com>
2016-12-08 21:57:27 -0500

    through step 4

--- lib/wumpus/room.rb
+++ lib/wumpus/room.rb
@@ -1,45 +1,72 @@
 module Wumpus
-  class Room
-    def initialize(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+    class Room
+        def initialize(number)
+            @number = number
+            @neighbors = []
+            @hazards = []
 
-    attr_reader :number, :neighbors
+            # raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
 
-    def add(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+        attr_reader :number, :neighbors
 
-    def remove(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+        def add(thing)
+            @hazards.push(thing)
 
-    def has?(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+            #     raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
 
-    def empty?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+        def remove(thing)
+            @hazards.delete(thing)
 
-    def safe?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+            #      raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
 
-    def connect(other_room)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+        def has?(thing)
+            @hazards.include?(thing)
 
-    def exits
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+            #      raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
 
-    def neighbor(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+        def empty?
+            @hazards.empty?
 
-    def random_neighbor
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+            # raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
+
+        def safe?
+            tmp = [@hazards.empty?]
+            @neighbors.each { |i| tmp.push(i.empty?) }
+            !tmp.include?(false)
+
+            #      raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
+
+        def connect(other_room)
+            @neighbors.push(other_room)
+            other_room.neighbors.push(self)
+
+            # raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
+
+        def exits
+            tmp = []
+            @neighbors.each { |i| tmp.push(i.number) }
+            tmp
+
+            # raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
+
+        def neighbor(number)
+            @neighbors[@neighbors.index { |i| i.number == number }]
+
+            # raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
+
+        def random_neighbor
+            @neighbors[Random.rand(@neighbors.length)]
+
+            # raise NotImplementedError, "See lib/wumpus/room.rb"
+        end
     end
-  end
 end
