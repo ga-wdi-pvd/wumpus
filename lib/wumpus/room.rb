01d5f31d147bdc2d54101865ed204b949c981b48
awanderlyss <alyssa.felix7@gmail.com>
2016-12-09 08:40:26 -0500

    Add solutions to the methods, still 2 errors 1 fail

--- lib/wumpus/room.rb
+++ lib/wumpus/room.rb
@@ -1,45 +1,58 @@
 module Wumpus
-  class Room
-    def initialize(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    attr_reader :number, :neighbors
-
-    def add(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def remove(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def has?(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def empty?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def safe?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def connect(other_room)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def exits
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def neighbor(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
-
-    def random_neighbor
-      raise NotImplementedError, "See lib/wumpus/room.rb"
-    end
+class Room
+  def initialize(number)
+    @number = number
+    @hazards = []
+    @neighbors = []
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
   end
+
+  attr_reader :number, :neighbors
+
+  def add(thing)
+    return @hazards.push(thing)
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def remove(thing)
+    return @hazards.delete(thing)
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def has?(thing)
+    return @hazards.include?(thing)
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def empty?
+    return @hazards.empty?
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def safe?
+    return empty?
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def connect(other_room)
+    neighbors.push(other_room)
+    other_room.neighbors.push(self)
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def exits
+
+    raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def neighbor(number)
+    neighbors.find(number)
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+
+  def random_neighbor
+    return neighbors.sample
+    # raise NotImplementedError, "See lib/wumpus/room.rb"
+  end
+end
 end
