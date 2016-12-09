a6c0fdb0f42298afe02d958cece6724c4e7c0d77
mconnors333 <matthew.connors333@gmail.com>
2016-12-09 08:53:39 -0500

    Connors Week04 Day04 HW

--- lib/wumpus/room.rb
+++ lib/wumpus/room.rb
@@ -1,45 +1,64 @@
+require 'pry'
+#adf
 module Wumpus
-  class Room
+
+  # A room with neighbors is not safe if its neighbors have hazards
+  # A room with neighbors is safe when it and its neighbors have no hazards
+  # A room with neighbor has two-way connection to neighbors
+  # A room with neighbors can choose a neighbor randomly
+  # A room with neighbors knows the numbers of all neighboring rooms
+  # A room is not safe if it has hazards
+class Room
     def initialize(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @number = number
+      @hazard = []
+      @neighbors = []
     end
 
     attr_reader :number, :neighbors
 
     def add(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @hazard.push(thing)
     end
 
     def remove(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @hazard.delete(thing)
     end
 
     def has?(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @hazard.include?(thing)
     end
 
     def empty?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @hazard.empty?
     end
 
     def safe?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      empty? && neighbors.all? do |neighbor|
+        neighbor.empty?
+      end
     end
 
     def connect(other_room)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @neighbors.push(other_room)
+      other_room.neighbors.push(self)
     end
 
     def exits
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      neighbors.map do |e|
+        e.number
+      end
     end
 
     def neighbor(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      neighbors.find do
+         |e| e.number == number
+       end
     end
 
     def random_neighbor
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      neighbors.sample
     end
   end
 end
+binding.pry
