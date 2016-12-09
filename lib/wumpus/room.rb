3f1b8e12bd73becc3df924b54ff7793b806a3aa5
yourUsername <your_email@example.com>
2016-12-09 13:46:13 -0500

    attempted hw

--- lib/wumpus/room.rb
+++ lib/wumpus/room.rb
@@ -1,45 +1,50 @@
 module Wumpus
   class Room
+
     def initialize(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      @number = number
     end
 
     attr_reader :number, :neighbors
 
-    def add(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+    def add(things)
+      # puts "There are ${things} in the room!"
+      @number
     end
 
     def remove(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def has?(thing)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def empty?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def safe?
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def connect(other_room)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def exits
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def neighbor(number)
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
     def random_neighbor
-      raise NotImplementedError, "See lib/wumpus/room.rb"
+      # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
   end
+
+room1 = Room.new(1)
+
 end
